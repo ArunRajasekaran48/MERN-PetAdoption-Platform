58cb8b977664440691a2e9b8bfebf0fa43302bb1
ArunRajasekaran48 <arun.r2023cse@sece.ac.in>
2025-07-25 23:29:19 +0530

    cors fix

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,7 +3,7 @@ import cors from "cors"
 import cookieparser from "cookie-parser"
 const app= express()
 
-const allowedOrigin = "http://localhost:5173"; 
+const allowedOrigin = "*"; 
 
 app.use(cors({
   origin: allowedOrigin,
